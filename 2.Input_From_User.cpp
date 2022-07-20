23acd1fd3d7dd8ac52e0c665788de3e0f8aeb089
suryabdr <90960783+suryabdr@users.noreply.github.com>
2022-07-20 21:03:30 +0545

    Add files via upload

---
+++ 2.Input_From_User.cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+ #include<string>
+ using namespace std;
+ int main()
+ {
+     string name;
+     int age;
+     cout<<"Enter your name and age: "<<endl;
+     cin>>name>>age;
+     cout<<"Your name is "<<name<<" and age is "<<age;
+     return 0;
+ }
